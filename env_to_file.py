e318765937b4ac35ed8420ae141f76cf9f5be3dd
pp-ps <pietro.stroia@pagopa.it>
2021-09-21 11:19:47 +0200

    implement base64 decoding

--- env_to_file.py
+++ env_to_file.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
 import argparse
+import base64
 import os
 import sys
 import textwrap
 
 
-def env_to_file(env_name, out):
+def env_to_file(env_name, out, base64_encoded):
     env_value = os.environ[env_name]
+    if base64_encoded is True:
+        env_value = base64.b64decode(env_value).decode("utf-8")
     with open(out, "w") as f:
         f.write(env_value)
 
@@ -24,10 +27,11 @@ def main(argv=None):
     )
     parser.add_argument("--env-name", required=True,
                         help="Environment variable name")
+    parser.add_argument("--base64", action="store_const", const=True)
     parser.add_argument("--out", required=True,
                         help="Output file")
     args = parser.parse_args(argv)
-    env_to_file(args.env_name, args.out)
+    env_to_file(args.env_name, args.out, args.base64)
 
 
 if __name__ == '__main__':
